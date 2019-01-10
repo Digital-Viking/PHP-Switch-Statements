fa7c7efec05c364709552b111fb46d953b102cc7
David Weston Webb <westwebb@gmail.com>
2019-01-10 11:10:00 -0500

    Create switch_statements.php

---
+++ switch_statements.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$number = 24;
+
+swtich($number) {
+
+    case 34:
+    echo "It is 34";
+      break;
+    case 37:
+    echo "It is 34";
+      break;
+    case 35:
+    echo "It is 35;
+      break;
+    case 24:
+    echo "It is 24;
+      break;
+      
+    default:
+    echo "We could not find anything";
+      break;
+      
+}
+
+?>
+
+</body>
+</html>
